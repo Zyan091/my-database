19f42d65cfdc2acb28eaf053f2af1c11eaefa957
Zyan091 <Kchou8456@gmail.com>
2026-04-20 10:55:16 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkg update -y && pkg upgrade -y
 pkg install python git -y
-git clone https://github.com/သင့်ရဲ့Username/သင့်ရဲ့Zyan 
-cd သင့်ရဲ့Zyan
+git clone https://github.com/Zyan091/my-database
+cd my-database
 python main.py
